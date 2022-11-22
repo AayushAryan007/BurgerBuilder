01aaa5d17e0268451a86244e7d621cb2bd463fef
Ayush <unitedestates.ayush10@gmail.com>
2022-11-22 21:50:16 +0530

    added non functional control panel

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Auxx";
 import Burger from "../../components/Layout/Burger/Burger";
+import BuildControls from "../../components/Layout/Burger/BuildControls/BuildControls";
 class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props);
@@ -18,7 +19,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
